90581a601c777b5bed1b32be9aa4dd4152c00f35
Jason Kao <jason.kao@columbia.edu>
2020-01-08 13:54:59 -0500

    cleaned up config docs

--- bin/config-docs.js
+++ bin/config-docs.js
@@ -23,15 +23,13 @@ fs.readFile(path.join(__dirname, '../keys/credentials.json'), (err, content) => 
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
       client_id, client_secret, redirect_uris[0]);
 
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, (err, token) => {
     if (err) return getNewToken(oAuth2Client);
-    // oAuth2Client.setCredentials(JSON.parse(token));
-    // callback(oAuth2Client);
   });
 }
 
@@ -55,13 +53,11 @@ function getNewToken(oAuth2Client) {
     rl.close();
     oAuth2Client.getToken(code, (err, token) => {
       if (err) return console.error('Error while trying to retrieve access token', err);
-      oAuth2Client.setCredentials(token);
       // Store the token to disk for later program executions
       fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
         if (err) return console.error(err);
         console.log('Token stored to', TOKEN_PATH);
       });
-      // callback(oAuth2Client);
     });
   });
 }
