91fcebde83ea1987ab33d45a2f23ff922e29177a
Jason Kao <jason.kao@columbia.edu>
2020-04-18 13:27:31 -0400

    Move ai2html resizer script from index.html into its own script; inititialization in page.js

--- templates/default/src/scripts/page.js
+++ templates/default/src/scripts/page.js
@@ -1,8 +1,74 @@
-/* Scripts that should be on every Spectate page */
+/**
+ * Put all initializer scripts into this init() function
+ */
+
+import initResizer from './ai2html-resizer';
+
+function init() {
+  initResizer();
+}
+
+/**
+ * Replace the section#main node with the article content
+ */
 
 const isOnSpectatorPage = window.location.host === 'www.columbiaspectator.com';
-const isOnContributorPage = isOnSpectatorPage &&
-  window.location.pathname.indexOf('/contributors') === 0;
+
+const SECTION_MAIN_SELECTOR = 'section#main';
+const ARTICLE_SELECTOR =
+  '.pb-f-article-article-body > .row > .col-xs-12 > .ab-article-body > .ab-article-content > article';
+
+// Replaces section#main with article
+function hoistArticle() {
+  // Replace section#main with article
+  const sectionMain = document.querySelector(SECTION_MAIN_SELECTOR);
+  const article = document.querySelector(ARTICLE_SELECTOR);
+  sectionMain.parentNode.replaceChild(article, sectionMain);
+
+  // Arc SSRs elements like links and meta tags in Spectate's index.html <head>
+  // into a paragraph, which takes up unwanted space thanks to Arc's CSS
+  const suspectParagraph = article.firstElementChild;
+  if (
+    [...suspectParagraph.children].some((el) =>
+      ['META', 'LINK'].includes(el.tagName),
+    )
+  ) {
+    suspectParagraph.style.margin = 0;
+  }
+
+  init();
+}
+
+// Runs hoistArticle() and stops RAF when necessary elements exist.
+// Stops after 5 seconds of trying.
+const TRY_TIME = 5000;
+let start = null;
+function ready(timestamp) {
+  if (document.body && document.querySelector(SECTION_MAIN_SELECTOR)) {
+    hoistArticle();
+    return;
+  }
+  if (timestamp - (start || (start = timestamp)) < TRY_TIME) {
+    // If the body element isn't found, run ready() again at the next frame
+    window.requestAnimationFrame(ready);
+  } else {
+    // After 5 seconds, stop requesting frames and just use window.onload
+    console.log(
+      'Gave up replacing %s with article after %dms. Using window.onload.',
+      SECTION_MAIN_SELECTOR,
+      TRY_TIME,
+    );
+    window.onload = hoistArticle;
+  }
+}
+
+// Initialize our ready() function.
+if (isOnSpectatorPage) {
+  window.requestAnimationFrame(ready);
+} else {
+  init();
+}
+
 
 /**
  * Disable stylesheets on contributor page. Stylesheets are included in the
@@ -10,63 +76,18 @@ const isOnContributorPage = isOnSpectatorPage &&
  * still override the site.
  */
 
+const isOnContributorPage =
+  isOnSpectatorPage && window.location.pathname.indexOf('/contributors') === 0;
+
 if (isOnContributorPage) {
   // Disable stylesheets
-  document.querySelectorAll('.story-summary > .twolines > link').forEach(link => link.disabled = true);
+  document
+    .querySelectorAll('.story-summary > .twolines > link')
+    .forEach((link) => (link.disabled = true));
+
   // Add styles to hide content preview
   const styleSheet = document.createElement('style');
   styleSheet.type = 'text/css';
   styleSheet.innerText = '.story-summary > .twolines { display: none; }';
   document.head.appendChild(styleSheet);
 }
-
-/**
- * Replace the section#main node with the article content
- */
-
-const SECTION_MAIN_SELECTOR = 'section#main';
-const ARTICLE_SELECTOR = '.pb-f-article-article-body > .row > .col-xs-12 > .ab-article-body > .ab-article-content > article';
-
-// Replaces section#main with article
-function hoistArticle() {
-  // Hoist article
-  const sectionMain = document.querySelector(SECTION_MAIN_SELECTOR);
-  const article = document.querySelector(ARTICLE_SELECTOR);
-  sectionMain.parentNode.replaceChild(article, sectionMain);
-
-  // Links and meta tags are SSR'd into this first paragraph,
-  // and it takes up unwanted space thanks to Arc's CSS
-  const suspectParagraph = article.firstElementChild;
-  if ([ ...suspectParagraph.children ].some(el => [ 'META', 'LINK' ].includes(el.tagName))) {
-    // If the paragraph includes <meta> or <link> tags, it's probably our SSR'd <head>
-    suspectParagraph.style.margin = 0;
-  }
-}
-
-// Runs hoistArticle() and stops RAF when necessary elements exist.
-// Stops after 5 seconds of trying.
-let start = null;
-function ready(timestamp) {
-  if (!start)
-    start = timestamp;
-  if (document.body && document.querySelector(SECTION_MAIN_SELECTOR)) {
-    hoistArticle();
-    return;
-  }
-  if (timestamp - start < 5000) {
-    // If the body element isn't found, run ready() again at the next frame
-    window.requestAnimationFrame(ready);
-  } else {
-    // After 5 seconds, stop requesting frames and just use window.onload
-    console.log(
-      'Gave up on hoisting article to' +
-      SECTION_MAIN_SELECTOR +
-      '. Using window.onload event.'
-    );
-    window.onload = hoistArticle;
-  }
-}
-
-// Initialize our ready() function.
-if (isOnSpectatorPage)
-  window.requestAnimationFrame(ready);
