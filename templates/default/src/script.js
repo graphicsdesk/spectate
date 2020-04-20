5508ab17d7a8a334204882df3f3690cd2ca42b4c
Jason Kao <jason.kao@columbia.edu>
2020-04-19 20:40:59 -0400

    news navbar and USE_NEWS_NAV setting; closes #11

--- templates/default/src/script.js
+++ templates/default/src/script.js
@@ -3,22 +3,24 @@ import textBalancer from 'text-balancer';
 import './scripts/page';
 
 import { spectate as spectateConfig } from '../package.json';
-const { USE_COVER_HED, USE_EYE_NAV } = spectateConfig;
+const { USE_NEWS_NAV, USE_EYE_NAV, USE_COVER_HED } = spectateConfig;
 
 // Fade in navbar at scroll trigger
 
 const navbar = document.getElementById('navbar');
 
-if (USE_COVER_HED || USE_EYE_NAV) {
+if (USE_NEWS_NAV || USE_EYE_NAV || USE_COVER_HED) {
   enterView({
-    selector: USE_COVER_HED ? '.headline' : '.step-deck',
-    offset: USE_COVER_HED ? 1 : 0.957,
+    selector: '.headline',
+    offset: 1,
     enter: () => {
-      navbar.classList.remove('only-logo');
+      navbar.classList.remove('only-eye-logo');
+      navbar.classList.remove('hide-news-navbar');
     },
     exit: () => {
       navbar.classList.remove('show-nav-links');
-      navbar.classList.add('only-logo');
+      navbar.classList.add('only-eye-logo');
+      navbar.classList.add('hide-news-navbar');
     },
   });
 }
